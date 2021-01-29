af4d55f3bc578ed8694ad69c53669fc74404ffa2
Chunye Wang <chunye.wang@xilinx.com>
2021-01-29 20:25:42 +0800

    update c.sh

--- c.sh
+++ c.sh
@@ -5,9 +5,10 @@ declare -a args
 function connection {
     args=();
     args+=(-L0.0.0.0:102$id:xbjlabdpsvr$id:22);
-    args+=(-L0.0.0.0:182$id:xbjlabdpsvr$id:8888);
+    args+=(-L0.0.0.0:182$id:xbjlabdpsvr$id:8080);
+    args+=(-L0.0.0.0:162$id:xbjlabdpsvr$id:6006);
     args+=(-R0.0.0.0:10074:xcdl190074:22);
-    args+=(-R0.0.0.0:10152:xsjsda15:22);
+    args+=(-R0.0.0.0:10152:xsjsda152:22);
     args+=(-R0.0.0.0:10142:xcosda142:22);
     args+=(-R0.0.0.0:10261:gitenterprise.xilinx.com:22);
     args+=(-oStrictHostKeyChecking=no)
